ab093d093a1aca03c42a6f57ba066f85c7360fe5
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-09-22 11:31:02 +0300

    Update ex1_3.py

--- ex1_3.py
+++ ex1_3.py
@@ -3,7 +3,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('num', type=str)
 args = parser.parse_args()
 l = list(args.num)
-for i, j in enumerate(l[:-1]):
+for i, j in enumerate(l):
     if ((j == '+' or j == '-') and j != l[i+1]) or j.isnumeric():
         k = True
     else:
