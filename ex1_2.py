9b2f7a758c2e2bd148556097ff1170c6684dc466
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-09-24 17:56:09 +0300

    Update ex1_2.py

--- ex1_2.py
+++ ex1_2.py
@@ -1,8 +1,15 @@
 import argparse
 import operator
+import math
 parser = argparse.ArgumentParser()
 parser.add_argument('op', type=str)
 parser.add_argument('num', type=int, nargs='+')
 args = parser.parse_args()
-function = getattr(operator, args.op)
-print(function(*args.num))
+if args.op in dir(math):
+    function = getattr(math, args.op)
+    print(function(*args.num))
+elif args.op in dir(operator):
+    function = getattr(operator, args.op)
+    print(function(*args.num))
+else:
+    print('There is no such math function')
