93dff54049a77f047ae1ba11b572f63bea00d8e8
Hundiak Valeriia <114095623+pojuipolynu@users.noreply.github.com>
2022-09-24 17:55:27 +0300

    Update ex1_1.py

--- ex1_1.py
+++ ex1_1.py
@@ -1,6 +1,12 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('num', type=str)
+parser.add_argument('num', type=int)
+parser.add_argument('op', type=str)
+parser.add_argument('num1', type=int)
 args = parser.parse_args()
-k = eval(args.num)
-print(k)
+if args.op not in ['+', '-', '*', '/']:
+    print('Invalid arithmetic operation')
+elif args.op == '/' and args.num1 == 0:
+    print('Dividing by zero')
+else:
+    print(eval(str(args.num)+args.op+str(args.num1)))
